675e91157a2a12199a3d44ba0ff45f98b6f16a07
cmarvann <covingtonmarvann@gmail.com>
2022-09-30 23:56:56 -0400

    fix path

--- Develop/gen.js
+++ Develop/gen.js
@@ -127,27 +127,6 @@ var upperCasedCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', '
   }
   
  
- 
-
-// function generatePassword() {
-
-//   console.log ("passwordgenerated");
-
-//   // add your logic here list of all possible values/characters for the pwd selection 
-//   let characters = "01234357890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&";
-//   // declaring var to store pwd lgth
-//   let passwordLength = Math.floor(Math.random() * 121) + 8;
-//   // declaring the var to store pwd
-//   let passwordValue = '';
-//   for (let i = 0; i < passwordLength; i++ ){
-//     let number = Math.floor(Math.random() * characters.length); 
-//     passwordValue += characters.substring(number, number + 1);
-
-//   }
-//   return passwordValue;
-// }
-
-
 // Write password to the #password input
 function writePassword() {
   var generatedPassword = generatePassword();
